8a53c8c4456ee40c6ccb602d61d4cffddeee8e80
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-04-11 13:20:22 -0300

    creating app

---
+++ php/loginTxt/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+  <title>Login TXT</title>
+</head>
+<body>
+  <?php
+    //Todo code
+  ?>
+  <script src="./js/jquery.js"></script>
+  <script src="./bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
