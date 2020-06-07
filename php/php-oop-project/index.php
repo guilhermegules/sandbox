a4241e8fa3d8b37add24b91d60d5181d1459b8eb
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-06-07 15:51:38 -0300

    feat: adding more tests on classes

--- php/php-oop-project/index.php
+++ php/php-oop-project/index.php
@@ -1,9 +1,10 @@
 <?php
-
+  // TODO adjust inheritance on UserAdmin
   class User {
 
     private $username;
     private $email;
+    private $role = "member";
 
     public function __construct($username, $email) {
       $this -> username = $username;
@@ -12,6 +13,14 @@
 
     public function addFriend() {
       return "$this->username added a new friend";
+    }
+
+    public function message() {
+      return "$this->email sent a new message";
+    }
+
+    public function getRole() {
+      return $this->role;
     }
 
     public function getEmail() {
@@ -35,12 +44,26 @@
 
   class AdminUser extends User {
     
-    public $level;
+    private $level;
+    private $role = "admin";
 
     public function __construct($username, $email, $level) {
       $this->level = $level;
       parent::__construct($username, $email);
     }
+
+    public function message() {
+      return "$this->email, an admin, sent a new message";
+    }
+
+    public function getLevel() {
+      return $this->level;
+    }
+
+    public function setLevel($level) {
+      $this->level = $level;
+    }
+
   }
 
   $userOne = new User('GuilhermeGM', 'guilhermegules@gmail.com');
@@ -49,7 +72,8 @@
 
   echo $userThree->getUsername() . "<br>";
   echo $userThree->getEmail() . "<br>";
-  echo $userThree->level . "<br>";
+  echo $userThree->getLevel() . "<br>";
+
   // print_r(get_class_vars('User')); method to get class vars
   // print_r(get_class_methods('User')); method to get class methods
 ?>
