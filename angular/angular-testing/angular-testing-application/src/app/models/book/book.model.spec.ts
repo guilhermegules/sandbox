1eca1cb1d4a0e3c5656897832b4d98667f0c90d2
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-06-08 22:38:47 -0300

    test: updating tests for book model

--- angular/angular-testing/angular-testing-application/src/app/models/book/book.model.spec.ts
+++ angular/angular-testing/angular-testing-application/src/app/models/book/book.model.spec.ts
@@ -1,4 +1,4 @@
-import { BookInterface, BookModel } from './book.model';
+import { BookModel } from './book.model';
 import * as faker from 'faker';
 
 describe('BookModel', () => {
@@ -7,17 +7,40 @@ describe('BookModel', () => {
   let description: string;
   let price: number;
   let upvotes: number;
+  let book: BookModel;
 
   beforeEach(() => {
     image = faker.image.image();
     title = faker.lorem.words();
-    description = faker.lorem.sentences();
+    description = faker.lorem.sentence();
     price = faker.commerce.price();
     upvotes = faker.random.number();
+    book = new BookModel(image, title, description, price, upvotes);
+
+    const storage = {};
+
+    spyOn(window.localStorage, 'getItem').and.callFake((key: string): string => {
+      return storage[key] || null;
+    });
+
+    spyOn(window.localStorage, 'removeItem').and.callFake((key: string): void => {
+      delete storage[key];
+    });
+
+    spyOn(window.localStorage, 'setItem').and.callFake((key: string, value: string): string => {
+      return storage[key] || null;
+    });
+
+    spyOn(window.localStorage, 'clear').and.callFake((): void => {
+      this.storage = {};
+    });
+
+    // afterEach(() => {
+    //   localStorage.clear();
+    // });
   });
 
   it('has a valid model', () => {
-    const book = new BookModel(image, title, description, price, upvotes);
     expect(book.image).toEqual(image);
     expect(book.title).toEqual(title);
     expect(book.description).toEqual(description);
