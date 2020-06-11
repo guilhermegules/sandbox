735ccb3e2d27d4ebd8f51400cd06378479471560
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-06-11 11:19:48 -0300

    feat(routes): adding routes to routing module

--- angular/angular-testing/angular-testing-application/src/app/app-routing.module.ts
+++ angular/angular-testing/angular-testing-application/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { BookEditComponent } from './components/book-edit/book-edit.component';
+import { BookComponent } from './components/book/book.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BooksListComponent } from './components/books-list/books-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'books/:title', component: BookComponent },
+  { path: 'books/:title/edit', component: BookEditComponent },
+  { path: 'books', component: BooksListComponent },
+  { path: '', redirectTo: 'books/', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
