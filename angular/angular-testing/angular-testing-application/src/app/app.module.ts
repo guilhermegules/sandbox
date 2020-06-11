1da70095200d4c4f25f652caf6c00cba3baa4872
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-06-11 11:20:20 -0300

    feat(modules): adding some modules to app.module

--- angular/angular-testing/angular-testing-application/src/app/app.module.ts
+++ angular/angular-testing/angular-testing-application/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookComponent } from './components/book/book.component';
+import { BooksListComponent } from './components/books-list/books-list.component';
+import { BookEditComponent } from './components/book-edit/book-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
+    BookComponent,
+    BooksListComponent,
+    BookEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
