2c97ebd21007c7bf9eb492752622420dd8df3ea7
Guilherme Gules Moreira <guilhermegules@gmail.com>
2021-07-11 21:29:24 -0300

    implementing array with java

---
+++ data-structures/README.md
@@ -0,0 +1,13 @@
+# Data structures with JAVA
+
+> All the data structures can be implemented on other languages
+
+## How Run
+
+- Run `./gradlew build` for build the project
+- Run `./gradlew test` for run all unit tests
+
+1. Array
+    - Simpler structure, called of inline structure
+    - Use indexes for a handle with positions
+    - In Java Arrays need to use predetermined positions number 
---
+++ data-structures/src/main/java/std/data/struct/util/Array.java
@@ -0,0 +1,70 @@
+package std.data.struct.util;
+
+import std.data.struct.model.Student;
+
+import java.util.Arrays;
+
+public class Array {
+    private Student[] students = new Student[100];
+    private int totalOfStudents = 0;
+
+    public void add(Student student) {
+        this.students[totalOfStudents] = student;
+        this.totalOfStudents++;
+    }
+
+    public void add(int index, Student student) {
+        if(!validPosition(index)) {
+            throw new IllegalArgumentException("Invalid position of array");
+        }
+
+        for(int i = totalOfStudents; i >= index; i--) {
+            students[i + 1] = students[i];
+        }
+
+        students[index] = student;
+        totalOfStudents++;
+    }
+
+    public Student get(int index) {
+        if(!occupiedPosition(index)) {
+            throw new IllegalArgumentException("Occupied position of array");
+        }
+
+        return students[index];
+    }
+
+    public void remove(int index) {
+        for (int i = index; i < this.totalOfStudents; i++) {
+            this.students[i] = this.students[i + 1];
+        }
+
+        this.totalOfStudents--;
+    }
+
+    public boolean have(Student student) {
+        for(int i = 0; i < totalOfStudents; i++) {
+            if (student.equals(students[i])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int size() {
+        return this.totalOfStudents;
+    }
+
+    public String toString() {
+        return Arrays.toString(students);
+    }
+
+    private boolean occupiedPosition(int index) {
+        return index >= 0 && index < totalOfStudents;
+    }
+
+    private boolean validPosition(int index) {
+        return index >= 0 && index <= totalOfStudents;
+    }
+}
