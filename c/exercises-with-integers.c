0470a7cd76f3be95742510bbb4ff21e5645f129b
Guilherme Gules Moreira <guilhermegules@gmail.com>
2021-08-03 22:41:28 -0300

    feat: adding integers exercises :x

---
+++ c/exercises-with-integers.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void exercise1() {
+	int numbers[100];
+	int i;
+
+	for (i = 0; i < 100; i++) {
+		numbers[i] = i + 1;
+
+		printf("[%d]\n", numbers[i] * 2);
+	}
+}
+
+void exercise2() {
+	int n = 12;
+	int acc = 0;
+
+	printf("Type a number: ");
+	scanf_s("%d", &n);
+
+	for (int i = 0; i <= n; i++) {
+		acc += i;
+	}
+
+	printf("Sum with all values %d", acc);
+}
+
+void exercise3() {
+	int n = 0;
+	int i = 0;
+	int odd = 1;
+
+	printf("Type a number to receive all the odds numbers: ");
+	scanf_s("%d", &n);
+	printf("---------");
+	
+	while (i < n) {
+		printf("\n%d\n", odd);
+		odd += 2;
+		i++;
+	}
+	
+	printf("---------");
+}
+
+void exercise4() {
+	int x = 0;
+	int n = 0;
+	int pow = 1;
+
+	printf("Type your number: ");
+	scanf_s("%d", &x);
+
+	printf("Type a number for elevate your number: ");
+	scanf_s("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		pow *= x;
+	}
+
+	printf("%d", pow);
+}
+
+void exercise5() {
+	struct Disc {
+		int quantity;
+		int sold_date;
+	} typedef Disc;
+
+	Disc disc;
+	disc.quantity = 0;
+	disc.sold_date = 0;
+
+	Disc all_sold_discs[10];
+
+	for (int i = 0; i < 10; i++) {
+		all_sold_discs[i].quantity = 10 * i;
+		all_sold_discs[i].sold_date = i++;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		if (disc.quantity < all_sold_discs[i].quantity) {
+			disc.quantity = all_sold_discs[i].quantity;
+			disc.sold_date = all_sold_discs[i].sold_date;
+		}
+	}
+
+	printf("\n \t Sold at day: %d \n \t Bigger quantity: %d", disc.sold_date, disc.quantity);
+}
+
+int main() {
+	// exercise1();
+	// exercise2();
+	// exercise3();
+	// exercise4();
+	exercise5();
+
+	return 1;
+}
+
