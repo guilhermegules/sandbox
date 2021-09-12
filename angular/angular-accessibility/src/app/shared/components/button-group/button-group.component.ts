74f32e40fb16899ec36e9ee0fd227d7e32150691
Guilherme Gules Moreira <guilhermegules@gmail.com>
2021-09-11 22:09:00 -0300

    feat: add more accessibility, defining role and aria-labelledby

--- angular/angular-accessibility/src/app/shared/components/button-group/button-group.component.ts
+++ angular/angular-accessibility/src/app/shared/components/button-group/button-group.component.ts
@@ -1,12 +1,28 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  forwardRef,
+  Input,
+  Output,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { YesNoValueEnum } from 'src/app/core/enum/button-group.enum';
+import { UniqueIdService } from '../../services/unique-id.service';
 
 @Component({
   selector: 'app-button-group',
   templateUrl: './button-group.component.html',
   styleUrls: ['./button-group.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      // Register a component of CSA before component exist
+      useExisting: forwardRef(() => ButtonGroupComponent),
+    },
+  ],
 })
-export class ButtonGroupComponent {
+export class ButtonGroupComponent implements ControlValueAccessor {
   @Output()
   public valueChange = new EventEmitter<string>();
 
@@ -17,9 +33,32 @@ export class ButtonGroupComponent {
   public label = '';
 
   public readonly OPTIONS = YesNoValueEnum;
+  public id: string = null;
+
+  constructor(uniqueIdService: UniqueIdService) {
+    this.id = uniqueIdService.generateUUIDWithPrefix('button-group');
+  }
+
+  public onChange = (value: string) => {};
+  public onTouched = () => {};
 
   public activate(value: string): void {
+    this.writeValue(value);
+  }
+
+  public writeValue(value: string): void {
     this.value = value;
+    this.onChange(value);
     this.valueChange.emit(this.value);
   }
+
+  public registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState?(isDisabled: boolean): void {}
 }
