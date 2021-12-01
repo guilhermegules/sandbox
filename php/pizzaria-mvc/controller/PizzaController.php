1481e061759f7bb671e64d844ca3f0bcadbd75d7
Guilherme Gules Moreira <guilhermegules@gmail.com>
2021-12-01 00:03:12 -0300

    feat: creating calculation request and pizza save

--- php/pizzaria-mvc/controller/PizzaController.php
+++ php/pizzaria-mvc/controller/PizzaController.php
@@ -5,14 +5,24 @@ require(__DIR__ . "/../external/ViewHandler.php");
 
 define("PIZZA_TYPE_CALABREZA", "Calabreza");
 define("PIZZA_TYPE_PEPERONI", "Peperoni");
-define("PIZZA_TYPE_FOUR_CHEESE", "4 Queijos");
+define("PIZZA_TYPE_FOUR_CHEESE", "Quatro queijos");
+
+define("ADD_PIZZA", "ADD");
+define("SAVE_PIZZA", "SAVE");
 
 class PizzaController
 {
   private Pizza $pizza;
   private ViewHandler $template;
   private string $pizzaType;
-  private string $pizzaPrice;
+  private float $pizzaPrice;
+  private string $pizzaAction;
+  private float $totalPrice = 0;
+  private int $pizzaCount = 0;
+  private float $bonus = 0;
+  private array $pizzaTypes = [];
+  private array $pizzaPrices = [];
+  private bool $hasBeenSaved = false;
 
   public function __construct()
   {
@@ -20,54 +30,24 @@ class PizzaController
     $this->template = new ViewHandler();
     $this->pizza = new Pizza();
     $this->pizzaType = $_POST['type'] ?? '';
-    $this->pizzaPrice = $_POST['price'] ?? '';
+    $this->pizzaPrice = $_POST['price'] ?? 0;
+    $this->pizzaAction = $_POST['submit'] ?? '';
   }
 
-  /**
-   * TODOS
-   * Add logic to more than five pizzas
-   * Add logic to more than fifteen pizzas
-   */
-  public function pizzaRequestTemplateHandler()
+  public function pizzaRequestHandler()
   {
-    if (isset($_SESSION['calculatedPrice'])) {
-      $calculatedPizzaPrice = $this->pizzaPriceCalcByType() + $_SESSION['calculatedPrice'];
-    } else {
-      $calculatedPizzaPrice = $this->pizzaPriceCalcByType();
+    switch ($this->pizzaAction) {
+      case SAVE_PIZZA:
+        $this->save();
+        break;
+      case ADD_PIZZA:
+        $this->addPizza();
+        break;
+      default:
+        $this->pizzaTypeAndPriceHandler();
+        $this->templateHandler();
+        break;
     }
-
-    $pizzaCount = 0;
-
-    if (isset($_SESSION['pizzaCount'])) {
-      $pizzaCount = $_SESSION['pizzaCount'] + 1;
-    }
-
-    $_SESSION['pizzaCount'] = $pizzaCount;
-    $_SESSION['calculatedPrice'] = $calculatedPizzaPrice;
-
-    $pizzas = $this->pizza->getPizzas();
-
-    $types = [];
-    $prices = [];
-
-    foreach ($pizzas as $key => $value) {
-      if ($key === 'tipo') {
-        array_push($types, $value);
-      }
-
-      if ($key === 'preco') {
-        array_push($prices, $value);
-      }
-    }
-
-    $this->template->getSmarty()->assign("title", "Realizar pedido");
-    $this->template->getSmarty()->assign("action", "O que vamos preparar?");
-    $this->template->getSmarty()->assign("name", "");
-    $this->template->getSmarty()->assign("types", $types);
-    $this->template->getSmarty()->assign("prices", $prices);
-    $this->template->getSmarty()->assign("calculatedPrice", $calculatedPizzaPrice);
-    $this->template->getSmarty()->assign("pizzaCount", $pizzaCount);
-    $this->template->getSmarty()->display('pizza-request.tpl');
   }
 
   private function pizzaPriceCalcByType()
@@ -82,5 +62,85 @@ class PizzaController
       default:
         return 0;
     }
+  }
+
+  private function save()
+  {
+    $userId = $_SESSION['login']['id'];
+    $pizzaCount = $_SESSION['pizzaCount'];
+    $totalPrice = $_SESSION['calculatedPrice'];
+    $bonus = $_SESSION['bonus'];
+
+    if ($this->pizzaCount >= 5) {
+      $this->hasBeenSaved = $this->pizza->savePizzaRequest($pizzaCount, $totalPrice, $userId, true);
+      $this->pizzaTypeAndPriceHandler();
+      $this->templateHandler();
+      return;
+    }
+
+    if ($this->pizzaCount > 15) {
+      $this->hasBeenSaved = $this->pizza->savePizzaRequest($pizzaCount, $totalPrice, $userId, true, $bonus);
+      $this->pizzaTypeAndPriceHandler();
+      $this->templateHandler();
+      return;
+    }
+
+    $this->hasBeenSaved = $this->pizza->savePizzaRequest($pizzaCount, $totalPrice, $userId);
+    $this->pizzaTypeAndPriceHandler();
+    $this->templateHandler();
+  }
+
+  private function addPizza()
+  {
+    if (isset($_SESSION['calculatedPrice'])) {
+      $this->totalPrice += $this->pizzaPriceCalcByType() + $_SESSION['calculatedPrice'];
+    } else {
+      $this->totalPrice += $this->pizzaPriceCalcByType();
+    }
+
+    if (isset($_SESSION['pizzaCount'])) {
+      $this->pizzaCount = $_SESSION['pizzaCount'] + 1;
+    }
+
+    $this->bonus = $this->totalPrice * 0.005;
+
+    $_SESSION['pizzaCount'] = $this->pizzaCount;
+    $_SESSION['calculatedPrice'] = $this->totalPrice;
+    $_SESSION['bonus'] = $this->bonus;
+
+    $this->pizzaTypeAndPriceHandler();
+    $this->templateHandler();
+  }
+
+  private function pizzaTypeAndPriceHandler()
+  {
+    $pizzas = $this->pizza->getPizzas();
+
+    foreach ($pizzas as $pizza) {
+      foreach ($pizza as $key => $value) {
+        if ($key === 'tipo') {
+          array_push($this->pizzaTypes, $value);
+        }
+
+        if ($key === 'preco') {
+          array_push($this->pizzaPrices, $value);
+        }
+      }
+    }
+  }
+
+  private function templateHandler()
+  {
+    $this->template->getSmarty()->assign("title", "Realizar pedido");
+    $this->template->getSmarty()->assign("action", "O que vamos preparar?");
+    $this->template->getSmarty()->assign("name", null);
+    $this->template->getSmarty()->assign("types", $this->pizzaTypes);
+    $this->template->getSmarty()->assign("prices", $this->pizzaPrices);
+    $this->template->getSmarty()->assign("calculatedPrice", $this->totalPrice);
+    $this->template->getSmarty()->assign("pizzaCount", $this->pizzaCount);
+    $this->template->getSmarty()->assign("hasBeenSaved", $this->hasBeenSaved);
+    $this->template->getSmarty()->assign("bonus", $this->bonus);
+    $this->template->getSmarty()->assign("user", $_SESSION['login']['username']);
+    $this->template->getSmarty()->display('pizza-request.tpl');
   }
 }
