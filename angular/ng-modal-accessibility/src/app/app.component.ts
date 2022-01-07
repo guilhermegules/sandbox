d8c0f8aef92b2b3b3c2434c10443ac537f5166b3
Guilherme Gules Moreira <guilhermegules@gmail.com>
2022-01-06 22:23:22 -0300

    feat: creating initial implementation with template

--- angular/ng-modal-accessibility/src/app/app.component.ts
+++ angular/ng-modal-accessibility/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { ModalRef } from './shared/modal/models/modal-ref';
+import { ModalService } from './shared/modal/services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-modal-accessibility';
+  @ViewChild('modal')
+  public modalTemplateRef!: TemplateRef<any>;
+
+  public modalRef!: ModalRef;
+
+  constructor(private modalService: ModalService) {}
+
+  public show(): void {
+    this.modalRef = this.modalService.open({ templateRef: this.modalTemplateRef as any, title: 'User details' });
+  }
 }
