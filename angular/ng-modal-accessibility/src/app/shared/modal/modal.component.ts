1ca08fa9f563964df92663a6c4e89b1b0be347f5
Guilherme Gules Moreira <guilhermegules@gmail.com>
2022-01-06 22:33:38 -0300

    feat: creating modal template and add config props

--- angular/ng-modal-accessibility/src/app/shared/modal/modal.component.ts
+++ angular/ng-modal-accessibility/src/app/shared/modal/modal.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { ModalConfig } from './interfaces/modal-config';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
 })
-export class ModalComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class ModalComponent {
+  public config!: ModalConfig;
 }
