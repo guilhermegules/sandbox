b4ec2cb69da1b2f72cb77148341d66d85008d9b6
Guilherme Gules Moreira <guilhermegules@gmail.com>
2022-01-16 19:12:51 -0300

    feat: creating directives for following wcag rules

--- angular/ng-modal-accessibility/src/app/shared/modal/modal.module.ts
+++ angular/ng-modal-accessibility/src/app/shared/modal/modal.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ModalComponent } from './modal.component';
 import { ModalService } from './services/modal.service';
+import { FocusTrapModule } from '../directives/focus-trap/focus-trap.module';
+import { FocusBackModule } from '../directives/focus-back/focus-back.module';
 
 @NgModule({
   declarations: [ModalComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FocusTrapModule, FocusBackModule],
   exports: [ModalComponent],
   providers: [ModalService],
 })
