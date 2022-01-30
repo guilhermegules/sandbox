a46b471cf411418e3dfe7a4892f1e57448db3dbb
Guilherme Gules Moreira <guilhermegules@gmail.com>
2022-01-30 19:44:48 -0300

    refactor: adjust app component and adding global styles

---
+++ react-native/gatito-alura-rn/App.tsx
@@ -0,0 +1,8 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import Services from './src/views/Services';
+
+export default function App() {
+  return <Services />;
+}
---
+++ react-native/gatito-alura-rn/src/variables.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  purple: '#a050be',
+  orange: '#fabe50',
+  darkGray: '#555555',
+  white: '#ffffff',
+  gray: '#c7c7c7',
+};
