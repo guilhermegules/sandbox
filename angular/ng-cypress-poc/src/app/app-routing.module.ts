33c15619fd5bd102e75cf87a51be6c7af458a2c0
Guilherme Gules Moreira <guilhermegules@gmail.com>
2022-04-01 14:32:20 -0300

    feat: add initial strucuture and creating counter module

--- angular/ng-cypress-poc/src/app/app-routing.module.ts
+++ angular/ng-cypress-poc/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'counter',
+    loadChildren: () =>
+      import('./modules/counter/counter.module').then((m) => m.CounterModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angular/ng-cypress-poc/src/app/app.module.ts
+++ angular/ng-cypress-poc/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, MainNavComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
