97eb30ac16378cca2118244f0623e8ff2c787ac8
Guilherme Gules Moreira <guilhermegules@gmail.com>
2022-04-03 19:22:04 -0300

    refactor(home and main-nav): removing unnecessary code

--- angular/ng-cypress-poc/src/app/components/main-nav/main-nav.component.ts
+++ angular/ng-cypress-poc/src/app/components/main-nav/main-nav.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.scss']
+  styleUrls: ['./main-nav.component.scss'],
 })
-export class MainNavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class MainNavComponent {}
