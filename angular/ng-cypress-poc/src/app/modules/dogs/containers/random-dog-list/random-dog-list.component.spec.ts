65d7f3d4a06046943fb440c83e639bc25df537ea
Guilherme Gules Moreira <guilhermegules@gmail.com>
2022-04-05 21:17:10 -0300

    feat(random-dog-list): updating component using data testid

--- angular/ng-cypress-poc/src/app/modules/dogs/containers/random-dog-list/random-dog-list.component.spec.ts
+++ angular/ng-cypress-poc/src/app/modules/dogs/containers/random-dog-list/random-dog-list.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RandomDogListComponent } from './random-dog-list.component';
@@ -8,9 +9,9 @@ describe('RandomDogListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RandomDogListComponent ]
-    })
-    .compileComponents();
+      declarations: [RandomDogListComponent],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
