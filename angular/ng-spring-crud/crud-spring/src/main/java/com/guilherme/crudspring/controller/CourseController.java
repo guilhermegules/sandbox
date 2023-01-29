c23aabb7464b7e0e86b4c974a0fc365b078d2cbd
Guilherme Gules <guilhermegules@gmail.com>
2023-01-29 13:38:59 -0300

    feat: create post mapping of courses

--- angular/ng-spring-crud/crud-spring/src/main/java/com/guilherme/crudspring/controller/CourseController.java
+++ angular/ng-spring-crud/crud-spring/src/main/java/com/guilherme/crudspring/controller/CourseController.java
@@ -2,8 +2,13 @@ package com.guilherme.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guilherme.crudspring.model.Course;
@@ -20,6 +25,12 @@ public class CourseController {
 
   @GetMapping
   public List<Course> list() {
-    return courseRepository.findAll();
+    return this.courseRepository.findAll();
+  }
+
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public Course create(@RequestBody Course course) {
+    return this.courseRepository.save(course);
   }
 }
