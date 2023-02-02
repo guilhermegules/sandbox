dde614eb0f53283417f465e41954d322dd0d02fa
Guilherme Gules <guilhermegules@gmail.com>
2023-02-02 18:34:52 -0300

    feat(CourseController): create find by id and update methods

--- angular/ng-spring-crud/crud-spring/src/main/java/com/guilherme/crudspring/controller/CourseController.java
+++ angular/ng-spring-crud/crud-spring/src/main/java/com/guilherme/crudspring/controller/CourseController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -32,5 +34,24 @@ public class CourseController {
   @ResponseStatus(code = HttpStatus.CREATED)
   public Course create(@RequestBody Course course) {
     return this.courseRepository.save(course);
+  }
+
+  @GetMapping("/{courseId}")
+  public ResponseEntity<Course> findById(@PathVariable Long courseId) {
+    return this.courseRepository.findById(courseId)
+      .map(course -> ResponseEntity.ok().body(course))
+      .orElse(ResponseEntity.notFound().build());
+  }
+
+  @PutMapping("/{courseId}")
+  public ResponseEntity<Course> update(@PathVariable Long courseId, @RequestBody Course course) {
+    return this.courseRepository.findById(courseId)
+      .map(courseRecord -> {
+        courseRecord.setName(course.getName());
+        courseRecord.setCategory(course.getCategory());
+        var updated = this.courseRepository.save(courseRecord);
+        return ResponseEntity.ok().body(updated);
+      })
+      .orElse(ResponseEntity.notFound().build());
   }
 }
