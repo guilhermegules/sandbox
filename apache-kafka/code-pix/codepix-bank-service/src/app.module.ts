79ed8f06b75f8b3e370189c267c4485cd93ae101
Guilherme Gules Moreira <guilhermegules@gmail.com>
2023-10-23 21:56:05 -0300

    feat: create pix keys service and controller

--- apache-kafka/code-pix/codepix-bank-service/src/app.module.ts
+++ apache-kafka/code-pix/codepix-bank-service/src/app.module.ts
@@ -1,24 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BankAccount } from './bank-accounts/entities/bank-account.entity';
+import { PixKeysModule } from './pix-keys/pix-keys.module';
+import { PixKey } from './pix-keys/entities/pix-key.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'db',
-      database: 'nest',
+      host: 'codepix-bank-service-db-1',
+      database: 'codepix',
       username: 'postgres',
       password: 'root',
       synchronize: true,
-      entities: [BankAccount],
+      entities: [BankAccount, PixKey],
     }),
     BankAccountsModule,
+    PixKeysModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
