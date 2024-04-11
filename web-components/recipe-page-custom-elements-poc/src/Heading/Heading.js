b0d4bcc5ff4e9375ef9d165aaed643b9070e19ae
Guilherme Gules <guilhermegules@gmail.com>
2024-04-10 22:31:31 -0300

    feat: add custom color for each title variant

--- web-components/recipe-page-custom-elements-poc/src/Heading/Heading.js
+++ web-components/recipe-page-custom-elements-poc/src/Heading/Heading.js
@@ -1,10 +1,20 @@
 export default class Heading extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: "open" });
     const variant = this.getAttribute("variant");
     const text = this.getAttribute("text");
-    this.shadowRoot.innerHTML = `<${variant}>${text}</${variant}>`;
+
+    shadow.innerHTML = `<${variant} style="color: ${this.getColor(
+      variant
+    )}">${text}</${variant}>`;
+  }
+
+  getColor(variant) {
+    if (variant === "h1") return "var(--black)";
+
+    if (variant === "h3") return "var(--primary-dark-raspberry)";
+
+    return "var(--primary-nutmeg)";
   }
 }
 
