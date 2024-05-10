8f4a0c640f197ef78246a09365291de1dbafc1fb
Guilherme Gules Moreira <guilherme.moreira@e-unicred.com.br>
2024-05-10 18:24:26 -0300

    chore(home): add tailwind config

--- single-spa/single-spa-white-wall/home/webpack.config.js
+++ single-spa/single-spa-white-wall/home/webpack.config.js
@@ -1,4 +1,4 @@
-const { merge } = require("webpack-merge");
+const { mergeWithRules } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 
 module.exports = (webpackConfigEnv, argv) => {
@@ -9,7 +9,30 @@ module.exports = (webpackConfigEnv, argv) => {
     argv,
   });
 
-  return merge(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
+  return mergeWithRules({
+    module: {
+      rules: {
+        test: "match",
+        use: "replace",
+      },
+    },
+  })(defaultConfig, {
+    // customize the webpack config here
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            require.resolve("style-loader", {
+              paths: [require.resolve("webpack-config-single-spa")],
+            }),
+            require.resolve("css-loader", {
+              paths: [require.resolve("webpack-config-single-spa")],
+            }),
+            "postcss-loader",
+          ],
+        },
+      ],
+    },
   });
 };
