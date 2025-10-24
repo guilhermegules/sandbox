91a33ffcd478138d89482b163ccc2ca325979519
Guilherme Gules Moreira <guilherme.moreira@stone.com.br>
2025-10-24 13:48:44 -0300

    feat: add factorial

---
+++ csharp/factorial/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Factorial(5));
+    }
+
+    private static int Factorial(int number)
+    {
+        if (number == 0) return 1;
+        return number * Factorial(number - 1);
+    }
+}
