9e16c09bc766eb3b4b881cc1b3efc08ca52482b7
Guilherme Gules Moreira <guilherme.moreira@stone.com.br>
2025-11-10 16:15:50 -0300

    feat: add functions and update the flow

--- devops/system-monitoring/system.sh
+++ devops/system-monitoring/system.sh
@@ -3,5 +3,36 @@
 LOG_DIR="monitoring-system"
 mkdir -p $LOG_DIR
 
-sudo grep -E "fail(ed)?|error|denied|unauthorized" /var/log/syslog | awk '{print $1, $2, $3, $5, $6, $7}' > $LOG_DIR/monitoring-logs.txt
+function logs_monitor() {
+	sudo grep -E "fail(ed)?|error|denied|unauthorized" /var/log/syslog | awk '{print $1, $2, $3, $5, $6, $7}' > $LOG_DIR/monitoring-logs.txt
+	sudo grep -E "fail(ed)?|error|denied|unauthorized" /var/log/auth.log | awk '{print $1, $2, $3, $5, $6, $7}' > $LOG_DIR/monitoring-logs-auth.txt
+}
 
+function network_monitor() {
+	if ping -c 1 8.8.8.8 > /dev/null; then
+		echo "$(date): Online" >> $LOG_DIR/monitoring-network.txt
+	else
+		echo "$(date): Offline" >> $LOG_DIR/monitoring-network.txt
+	fi
+
+	if curl -s --head https://www.alura.com.br/ | grep "HTTP/2 200" > /dev/null; then
+		echo "$(date): Connected with Alura" >> $LOG_DIR/monitoring-network.txt
+	else 
+		echo "$(date): Alura Offline" >> $LOG_DIR/monitoring-network.txt
+	fi
+}
+
+function disk_monitor() {
+	echo "$(date)" >> $LOG_DIR/disk-monitoring.txt
+	df -h | awk '$5+0 > 50 {print $1 " is with " $5 " of usage."}' >> $LOG_DIR/disk-monitoring.txt
+	echo "Disk usage on the home directory" >> $LOG_DIR/disk-monitoring.txt
+	du -sh /home/guilherme.moreira >> $LOG_DIR/disk-monitoring.txt
+}
+
+function main() {
+	logs_monitor
+	network_monitor
+	disk_monitor
+}
+
+main
