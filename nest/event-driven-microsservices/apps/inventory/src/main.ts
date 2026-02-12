a7933560b0df8179ab1f6c272eccfd1b118b1224
Guilherme Gules Moreira <guilhermegules@gmail.com>
2026-02-12 17:10:47 -0300

    feat: create microsservice initial structure

--- nest/event-driven-microsservices/apps/inventory/src/main.ts
+++ nest/event-driven-microsservices/apps/inventory/src/main.ts
@@ -4,10 +4,10 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.REDIS,
+    transport: Transport.TCP,
     options: {
-      url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`,
-      port: Number(process.env.PORT)
+      // url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`,
+      port: 3000
     }
   });
   await app.listen().then(() => console.log('Inventory microservice is running'));
