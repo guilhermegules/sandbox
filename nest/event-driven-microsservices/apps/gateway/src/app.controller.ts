caf53b36ee4a0f65f2da8bbe6cd01568186c6db8
Guilherme Gules Moreira <guilhermegules@gmail.com>
2026-02-16 19:58:12 -0300

    feat: move controller message to service

--- nest/event-driven-microsservices/apps/gateway/src/app.controller.ts
+++ nest/event-driven-microsservices/apps/gateway/src/app.controller.ts
@@ -1,21 +1,15 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
 import { lastValueFrom } from 'rxjs';
+import { AppService } from './app.service';
 
 @Controller("api/gateway/orders")
 export class AppController {
-   constructor(
-    @Inject('ORDERS_SERVICE')
-    private ordersClient: ClientProxy,
-  ) {}
+  constructor(
+    private service: AppService,
+  ) { }
 
   @Post()
   async createOrder(@Body() body: any) {
-    return lastValueFrom(
-      this.ordersClient.send(
-        { cmd: 'create-order' },
-        body,
-      ),
-    );
+    return this.service.createOrder(body)
   }
 }
--- nest/event-driven-microsservices/apps/gateway/src/app.service.ts
+++ nest/event-driven-microsservices/apps/gateway/src/app.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject('ORDERS_SERVICE')
+    private ordersClient: ClientProxy,
+  ) { }
+
+  createOrder(orderCreated: any) {
+    return lastValueFrom(
+      this.ordersClient.send(
+        { cmd: 'create-order' },
+        orderCreated,
+      ),
+    );
   }
 }
