1a8d2c41196977307e29ec3d30b08c6622d19a99
Guilherme Gules Moreira <guilhermegules@gmail.com>
2026-02-24 13:36:44 -0300

    feat: add day 2 solution

--- algorithms/advent-of-code/2025/src/main/java/Main.java
+++ algorithms/advent-of-code/2025/src/main/java/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         final var day1 = new Day1();
         day1.solution();
+        final var day2 = new Day2();
+        day2.solution();
     }
 }
