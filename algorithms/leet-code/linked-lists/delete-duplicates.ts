29962e6b92cb73c91f03b9220c229f335d3774f6
Guilherme Gules Moreira <guilhermegules@gmail.com>
2026-03-09 12:04:13 -0300

    feat: add delete duplicated with linked lists

---
+++ algorithms/leet-code/linked-lists/delete-duplicates.ts
@@ -0,0 +1,22 @@
+import { ListNode } from './list-node';
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let current: ListNode | null = head;
+    while (current && current.next) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+
+    return head;
+};
+
+const list = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))))
+
+console.log(deleteDuplicates(list))
+
+const list2 = new ListNode(1, new ListNode(1, new ListNode(2)))
+
+console.log(deleteDuplicates(list2))
